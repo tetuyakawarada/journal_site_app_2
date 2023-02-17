e1086518d2d274d38ede7347cf0d4bb481bedfcf
tetuyakawarada <tetuya.kawarada@gmail.com>
2023-02-17 09:02:43 +0900

    モーダル追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').js( 'resources/js/sample.js', 'public/js' ).autoload( {"jquery": [ '$', 'window.jQuery' ] } )
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
